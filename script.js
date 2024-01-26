83dd9f28401c68b6a9b7dc3a8c0b3b355a0e2d8f
Mike Costa <mcosta1@gmail.com>
2024-01-26 16:06:09 -0500

    Remove obsolete code for UI version of the game
    The functions getPlayerChoice and game are no longer needed.

--- script.js
+++ script.js
@@ -11,19 +11,6 @@ function getComputerChoice() {
   }
 }
 
-function getPlayerChoice() {
-  let choice;
-
-  do {
-    choice = prompt("Enter Rock, Paper, or Scissors:");
-    // This is needed in case the player clicks the 'Cancel' button
-    choice = choice ?? '';
-    choice = choice.trim().toLowerCase();
-  } while(choice !== 'rock' && choice !== 'paper' && choice !== 'scissors');
-
-  return choice.charAt(0).toUpperCase() + choice.slice(1);
-}
-
 /*
   Returns:
     1 if the player wins
@@ -48,44 +35,4 @@ function playRound(playerChoice, computerChoice) {
   }
 
   return -1;
-}
-
-function game() {
-  let playerPoints = 0;
-  let computerPoints = 0;
-  let ties = 0;
-
-  for(let i = 0; i < 5; i++) {
-    let playerChoice = getPlayerChoice();
-    let computerChoice = getComputerChoice();
-
-    console.log(`Player has chosen: ${playerChoice}`);
-    console.log(`Computer has chosen: ${computerChoice}`);
-
-    let result = playRound(playerChoice, computerChoice);
-
-    if(result > 0) {
-      console.log('Player wins this round');
-      playerPoints++;
-    } else if(result < 0) {
-      console.log('Computer wins this round');
-      computerPoints++;
-    } else {
-      console.log('This round is a tie');
-      ties++;
-    }
-  }
-
-  console.log('Final Score:\n');
-  console.log(`    Player: ${playerPoints}`);
-  console.log(`  Computer: ${computerPoints}`);
-  console.log(`      Ties: ${ties}\n`);
-  
-  if(playerPoints > computerPoints) {
-    console.log('Player Wins!');
-  } else if(playerPoints < computerPoints) {
-    console.log('Computer Wins');
-  } else {
-    console.log('The game is a tie');
-  }
 }
